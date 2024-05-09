f2b9004692d38310e4619b8407d39d3e2274f308
Daniel-TheProgrammer <njid18753@gmail.com>
2024-05-09 19:54:21 +0100

    chore: init and build nav

---
+++ app/components/nav.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export const Nav = () => {
+  return (
+    <>
+      <div>
+        <div className="flex container items-center mx-auto min-h-[116px] justify-between ">
+          <Image src="/assets/Logo.png" alt="logo" width={140} height={48} />
+          <ul className="flex items-center gap-[32px]">
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">Home </Link>
+            </li>
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">Product</Link>
+            </li>
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">Pricing</Link>
+            </li>
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">About Us</Link>
+            </li>
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">Blog</Link>
+            </li>
+            <li className="text-[#1D2130] leading-[150%]  text-[16px]">
+              <Link href="#">Contact</Link>
+            </li>
+            <button className="outline-none border-none min-h-[56px] w-[144px] items-center justify-center rounded-[12px] flex text-center itemss-center bg-[#1D2130] text-white leading-[150%] text-[16px]">
+              Free trial
+            </button>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
